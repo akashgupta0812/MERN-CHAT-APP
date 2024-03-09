8ff7a1ebd08d8dd560c891e93665ae781fa51314
akashgupta0812 <akashgupta2003.com@gmail.com>
2024-03-10 00:16:02 +0530

    changes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ try{
            
     console.log("req.body toekn ",req.body.token);
     console.log("req.cookies.token ",req.cookies.token);
-    console.log(req.header("Authorization").replace("Bearer ",""))
+    console.log("header se",req.header("Authorization").replace("Bearer ",""))
           const token=req.body.token||req.cookies.token||req.header("Authorization").replace("Bearer ","");
 
 if(!token)
