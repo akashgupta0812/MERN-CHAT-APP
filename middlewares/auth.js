12245c9ffb5140acd8eeee448a571ed3ce68deb3
akashgupta0812 <akashgupta2003.com@gmail.com>
2024-03-10 00:41:00 +0530

    Changes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@
 const jwt=require("jsonwebtoken")
 exports.auth=async (req,res,next)=>{
 try{
-           
+           console.log("req .body mei kya hai ",req.body);
     console.log("req.body toekn ",req.body.token);
     console.log("req.cookies.token ",req.cookies.token);
     // console.log("header se",req.header("Authorization").replace("Bearer ",""))
