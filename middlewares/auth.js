ee96d726c772eb1e46c025d3f3533b8a565362f8
akashgupta0812 <akashgupta2003.com@gmail.com>
2024-03-10 09:22:31 +0530

    changes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,44 +1,73 @@
-//Admin
-const jwt=require("jsonwebtoken")
-exports.auth=async (req,res,next)=>{
-try{
+// //Admin
+// const jwt=require("jsonwebtoken")
+// exports.auth=async (req,res,next)=>{
+// try{
          
     
-    
-    // console.log("header se",req.header("Authorization").replace("Bearer ",""))
-          const token=req.body.token||req.cookies.token
-          ||
-          req.header("Authorization").replace("Bearer ","");
+  
+//     // console.log("header se",req.header("Authorization").replace("Bearer ",""))
+//           const token=req.body.token||req.cookies.token
+//           ||
+//           req.header("Authorization").replace("Bearer ","");
 
-if(!token)
-{
-    return res.json({
-        success:false,
-        message:"Token is not verified"
-    })
-}
-try{
-    const decode= jwt.verify(token,"Akash")
-    // console.log(decode);
-    // console.log(req);
-    req.user=decode;
-    // console.log("req ki body  is ",req.user);
-}
-catch(err)
-{
-    return res.json({
-        success:false,
-        message:err.message
-    })
-}
-next();
-}catch(err)
-{
-return res.status(400).json({
-    success:false,
-    message:err.message
-})
-}
-}
+// if(!token)
+// {
+//     return res.json({
+//         success:false,
+//         message:"Token is not verified"
+//     })
+// }
+// try{
+//     const decode= jwt.verify(token,"Akash")
+//     // console.log(decode);
+//     // console.log(req);
+//     req.user=decode;
+//     // console.log("req ki body  is ",req.user);
+// }
+// catch(err)
+// {
+//     return res.json({
+//         success:false,
+//         message:err.message
+//     })
+// }
+// next();
+// }catch(err)
+// {
+// return res.status(400).json({
+//     success:false,
+//     message:err.message
+// })
+// }
+// }
 
 
+const jwt = require("jsonwebtoken");
+
+exports.auth = async (req, res, next) => {
+    try {
+        let token = req.header("Authorization");
+
+        if (!token) {
+            token = req.body.token || req.cookies.token;
+        } else {
+            token = token.replace("Bearer ", "");
+        }
+
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Token is not provided"
+            });
+        }
+
+        const decoded = jwt.verify(token, "Akash");
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(401).json({
+            success: false,
+            message: err.message
+        });
+    }
+};
